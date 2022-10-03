ad52a81d6313bf6c7000bac82527a69d858a513d
saifullah0317 <2020cs102@student.uet.edu.pk>
2022-10-03 17:42:06 +0500

    18-24

---
+++ ConditionalTests.js
@@ -0,0 +1,13 @@
+let car = 'subaru'
+var list=['a','b']
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+console.log(1==1)
+console.log(2/2==1)
+console.log('saif'=='saif')
+console.log(Array.isArray(list))
+console.log(car!='subaru')
+console.log(1==2)
+console.log(4/2==1)
+console.log('saif'=='Saif')
+console.log(Array.isArray(car))
---
+++ IntentionalError.js
@@ -0,0 +1,5 @@
+var mountains=['everest','k2']
+var rivers=['neelum','farat']
+var cities=['Islamabad','Newyork']
+var list = mountains.concat(rivers, cities);
+console.log(list[6])
